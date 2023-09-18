32bcbf61538ab114599c6ad646750047c9bc1f01
tingzhu.wang <tingzhu.wang@sophgo.com>
2023-09-18 11:36:10 +0800

    sg2260:add sg2260 platform file
    Signed-off-by: tingzhu.wang <tingzhu.wang@sophgo.com>

---
+++ platform/generic/sophgo/objects.mk
@@ -0,0 +1,6 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+carray-platform_override_modules-$(CONFIG_PLATFORM_SOPHGO_SG2260) += sophgo_sg2260
+platform-objs-$(CONFIG_PLATFORM_SOPHGO_SG2260) += sophgo/sg2260.o
