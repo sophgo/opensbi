08c8572c8d14debb0aba55106d68e82e8b76b60a
tingzhu.wang <tingzhu.wang@sophgo.com>
2023-09-18 11:36:10 +0800

    platform: generic: add sophgo sg2260 soc support
    Signed-off-by: tingzhu.wang <tingzhu.wang@sophgo.com>
    Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

--- platform/generic/sophgo/objects.mk
+++ platform/generic/sophgo/objects.mk
@@ -7,3 +7,5 @@
 
 carray-platform_override_modules-$(CONFIG_PLATFORM_SOPHGO_SG2042) += sophgo_sg2042
 platform-objs-$(CONFIG_PLATFORM_SOPHGO_SG2042) += sophgo/sg2042.o
+carray-platform_override_modules-$(CONFIG_PLATFORM_SOPHGO_SG2260) += sophgo_sg2260
+platform-objs-$(CONFIG_PLATFORM_SOPHGO_SG2260) += sophgo/sg2260.o
